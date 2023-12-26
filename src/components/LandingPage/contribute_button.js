c85501217402c2d1c6a52dc3b7eb4cd000334119
Kaushal Aggarwal <k.aggarwal@salesforce.com>
2023-12-26 20:57:36 +0530

    init redux

--- src/components/LandingPage/contribute_button.js
+++ src/components/LandingPage/contribute_button.js
@@ -1,11 +1,12 @@
 import "../../styles/landing_page.css";
-import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
 
-function ContributeButton(){
-    return <div className="contribute-button-box">
-        <LibraryBooksIcon className="contribute-button-content" />
-        <p className="contribute-button-content">Contribute</p>
-    </div>
+function ContributeButton() {
+  return (
+      <div className="contribute-button-box" >
+        <p className="contribute-button-content">Login</p>
+      </div>
+  );
 }
 
 export default ContributeButton;
